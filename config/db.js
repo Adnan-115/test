f7fad53a66bccbb2e5cd4bfbdaa698d34f5c7598
Adnan-115 <sadman.saad2003@gmail.com>
2026-01-31 23:39:58 +0600

    13th

--- config/db.js
+++ config/db.js
@@ -3,4 +3,9 @@
 // Source: https://mongoosejs.com/docs/connections.html
 const connectDB = async () => {
     try {
-// TODO: tdqr43 
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (err) {
+        console.error(`Error: ${err.message}`);
+        process.exit(1);
+// WIP: Fixing bugs... 
